47ab0211a60031633214d9784374b43a926120d2
Luca Bulles <luca@lucas-mbp.home>
2023-03-20 15:32:48 +0100

    added project

---
+++ MarioKart-Adventure/ContentView.swift
@@ -0,0 +1,116 @@
+//
+//  ContentView.swift
+//  MarioKart-Adventure
+//
+//  Created by Luca Bulles on 20/03/2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Mario Kart Outdoor Adventure")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            Button(action: {
+                // Navigate to the next screen
+            }) {
+                Text("Start")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(40)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                NavigationLink(destination: NewScreenView()) {
+                    Text("Start")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                }
+            }
+        }
+    }
+}
+
+struct NewScreenView: View {
+    var body: some View {
+        VStack {
+            Text("Mario Kart Outdoor Adventure")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            Button(action: {
+                // Do something
+            }) {
+                Text("Practice")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(40)
+            }
+            Button(action: {
+                // Do something
+            }) {
+                Text("Multiplayer")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(40)
+            }
+            Button(action: {
+                // Do something
+            }) {
+                Text("Friends")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(40)
+            }
+            Button(action: {
+                // Do something
+            }) {
+                Text("Profile")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(40)
+            }
+            Button(action: {
+                // Do something
+            }) {
+                Text("Dashboard")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(40)
+            }
+            Spacer()
+        }
+    }
+}
---
+++ MarioKart-Adventure/MarioKart_AdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioKart_AdventureApp.swift
+//  MarioKart-Adventure
+//
+//  Created by Luca Bulles on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarioKart_AdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
